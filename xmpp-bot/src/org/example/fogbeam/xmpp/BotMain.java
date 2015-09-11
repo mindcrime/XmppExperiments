7d05331144c5d5693899dbd3366f8d4be2cc7c9a
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-09-11 19:25:56 -0400

    make sure autoReconnect is enabled, add a ConnectionListener

--- xmpp-bot/src/org/example/fogbeam/xmpp/BotMain.java
+++ xmpp-bot/src/org/example/fogbeam/xmpp/BotMain.java
@@ -4,6 +4,9 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.ConnectionListener;
+import org.jivesoftware.smack.ReconnectionManager;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.chat.Chat;
 import org.jivesoftware.smack.chat.ChatManager;
 import org.jivesoftware.smack.chat.ChatManagerListener;
@@ -17,11 +20,10 @@ public class BotMain
 	{
 		
 		XMPPTCPConnectionConfiguration.Builder configBuilder = XMPPTCPConnectionConfiguration.builder();
-		configBuilder.setUsernameAndPassword("testuser1", "password");
+		configBuilder.setUsernameAndPassword("ciencia", "Bradbury451!");
 		configBuilder.setResource("SomeResource");
 		configBuilder.setServiceName("fogbeam.org");
 		configBuilder.setHost( "www.fogbeam.org" );
-		
 		
 		// accept all certificate - just for testing  
 		try 
@@ -36,29 +38,112 @@ public class BotMain
 		}  
 		
 		AbstractXMPPConnection connection = new XMPPTCPConnection(configBuilder.build());
-		// Connect to the server
-		connection.connect();
 		
-		// Log into the server
-		connection.login();
-		
-		ChatManager chatManager = ChatManager.getInstanceFor(connection);
-		chatManager.addChatListener(
-			new ChatManagerListener() {
-				@Override
-				public void chatCreated(Chat chat, boolean createdLocally)
-				{
-					if (!createdLocally)
-						chat.addMessageListener(new MyNewMessageListener());;
-				}
-			});		
-		
-		System.out.println( "running..." );
-
-		while( true )
-		{
-			Thread.sleep( 100000 );
+	
+		class MyConnectionListener  implements ConnectionListener {
 			
+			boolean m_reconnect = false;
+			
+			public boolean isReconnect()
+			{
+				return m_reconnect;
+			}
+			
+			@Override
+			public void connectionClosedOnError(Exception arg0) {
+				System.out.println( "connectionClosedOnError called!");
+				m_reconnect = true;
+				Thread.currentThread().interrupt();
+			}
+
+			@Override
+			public void authenticated(XMPPConnection arg0, boolean arg1) {
+				System.out.println( "authenticated called!");
+				
+			}
+
+			@Override
+			public void connected(XMPPConnection arg0) {
+				System.out.println( "connected called!");
+				
+			}
+
+			@Override
+			public void connectionClosed() {
+				System.out.println( "connectionClosed called!");
+				
+			}
+
+			@Override
+			public void reconnectingIn(int arg0) {
+				System.out.println( "reconnectingIn called!");
+				
+			}
+
+			@Override
+			public void reconnectionFailed(Exception arg0) {
+				System.out.println( "reconnectionFailed called!");
+				
+			}
+
+			@Override
+			public void reconnectionSuccessful() {
+				System.out.println( "reconnectionSuccessful called!");
+				
+			}
+		};		
+		
+		
+		MyConnectionListener connectionListener = new MyConnectionListener();
+
+		// make sure auto reconnect is turned on.
+		ReconnectionManager.getInstanceFor(connection).enableAutomaticReconnection();
+		
+		try
+		{				
+			System.out.println( "Connecting...");
+				
+					
+			connection.addConnectionListener( connectionListener );
+			
+			// Connect to the server
+			connection.connect();
+	
+			// Log into the server
+			connection.login();
+	
+			ChatManager chatManager = ChatManager.getInstanceFor(connection);
+			chatManager.addChatListener(
+				new ChatManagerListener() {
+					@Override
+					public void chatCreated(Chat chat, boolean createdLocally)
+					{
+						if (!createdLocally)
+							chat.addMessageListener(new MyNewMessageListener());;
+					}
+				});	
+		
+			
+			System.out.println( "running..." );
+			
+			while( true )
+			{
+				try
+				{
+					Thread.sleep( 1000000 );
+				}
+				catch( InterruptedException e )
+				{
+					e.printStackTrace();
+				}
+			}
 		}
+		catch( Exception e )
+		{
+			System.err.println( "Uh oh... something broke!");
+			e.printStackTrace();
+		}
+
+		
 	}
 }
